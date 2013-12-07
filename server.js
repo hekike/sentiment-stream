02957293fdffbef86fe0f5f32209e8dd7e133957
Peter Marton <email@martonpeter.com>
2013-12-07 21:10:19 +0100

    delete unnec

--- server.js
+++ server.js
@@ -53,11 +53,3 @@ twitter
   .on('data', function (data) {
     io.sockets.emit('tweet', data);
   });
-
-
-io.sockets.on('connection', function (socket) {
-  socket.emit('news', { hello: 'world' });
-  socket.on('my other event', function (data) {
-    console.log(data);
-  });
-});
